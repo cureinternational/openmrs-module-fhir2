ec021d872aa7a95f5daf5a8d791c6eb01d1abb24
Vivek Kumar Pandey <vivekp@thoughtworks.com>
2026-06-08 12:21:58 +0530

    HIVE-111216: Vivek: address PR review findings

--- api/src/main/java/org/openmrs/module/fhir2/api/dao/impl/FhirTaskDaoImpl.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/dao/impl/FhirTaskDaoImpl.java
@@ -51,7 +51,7 @@ public class FhirTaskDaoImpl extends BaseFhirDao<FhirTask> implements FhirTaskDa
 					break;
 				case FhirConstants.FOCUS_REFERENCE_SEARCH_HANDLER:
 					entry.getValue().forEach(param -> handleReference(criteria, (ReferenceAndListParam) param.getParam(),
-					    "focusReference", "focr"));
+					    "focusReference", "fo"));
 					break;
 				case FhirConstants.TASK_CODE_SEARCH_HANDLER:
 					entry.getValue().forEach(code -> handleTaskCodeConcept(criteria, (TokenAndListParam) code.getParam()));
